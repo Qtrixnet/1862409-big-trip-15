b2ab8842e3fee5f5f2ac76026c340c610971e7a5
Kirill Shashichev <67040346+Qtrixnet@users.noreply.github.com>
2021-07-27 20:27:47 +0600

    исправить ошибку devServer

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,12 @@ const path = require('path')
 
 module.exports = {
   entry: './src/main.js',
-  module: {
-    rules: []
-  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   devtool: 'source-map',
-  devserver: {
+  devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     watchContentBase: true,
   }

5bbd40eae0b550e4f4b4b6928924751e8f30f040
Kirill Shashichev <67040346+Qtrixnet@users.noreply.github.com>
2021-07-29 20:19:33 +0600

    добавить в хедер информацию о путешествии

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import { createNavigationTemplate } from './view/site-menu'
+import { createTripInfoTemplate } from './view/trip-info'
 
+//* Функция рендера блоков
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 }
 
+//* Меню навигации
 const headerElement = document.querySelector('.page-header'),
-navigationElement = headerElement.querySelector('.trip-controls__navigation');
+  navigationElement = headerElement.querySelector('.trip-controls__navigation');
 
 render(navigationElement, createNavigationTemplate(), 'beforeEnd')
+
+//* Информация о путешествии (Маршрут и стоимость)
+const tripMainElement = document.querySelector('.trip-main');
+render(tripMainElement, createTripInfoTemplate(), 'afterbegin')
